8d4108a11b6010af6725053620207b34f7dc84a2
Gabriel Aguiar De La Rocque <gde-la-r@c2r2s10.42porto.com>
2024-12-16 17:27:01 +0000

    rules.c struct done

--- Makefile
+++ Makefile
@@ -6,12 +6,30 @@
 #    By: gde-la-r <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/12/16 14:20:05 by gde-la-r          #+#    #+#              #
-#    Updated: 2024/12/16 14:21:10 by gde-la-r         ###   ########.fr        #
+#    Updated: 2024/12/16 16:18:48 by gde-la-r         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME=push_swap.a
 
-CC=cc -Wall -Wextre -Werror
+CC=cc
 
+CFLAGS= -Wall -Wextra -Werror
 
+INCLUDES=./includes/push_swap.h
+
+SRC_DIR=./sources
+
+SRC= $(SRC_DIR)/main.c \
+	 $(SRC_DIR)/rules.c \
+	 $(SRC_DIR)/errors.c \
+
+OBJ= $(SRC: .c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	ar rcs $(NAME) $(OBJS)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -I $(INCLUDES)
---
+++ sources/rules.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rules.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gde-la-r <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/16 17:22:53 by gde-la-r          #+#    #+#             */
+/*   Updated: 2024/12/16 17:26:43 by gde-la-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include ".././includes/push_swap.h"
+
+t_stack	*sa(t_stack	*a)
+{
+}
+
+t_stack *sb(t_stack *b)
+{
+}
+
+t_stack	*pa(t_stack *a, t_stack *b)
+{
+}
+
+t_stack	*pb(t_stack *a, t_stack *b)
+{
+}
+
+t_stack *ra(t_stack *a)
+{
+}
+
+t_stack *rb(t_stack *b)
+{
+}
+
+t_stack	*rra(t_stack *a)
+{
+}
+
+t_stack *rrb(t_stack *b)
+{
+}
