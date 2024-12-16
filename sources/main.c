962a671cd142e1c76cea58f64070d9c65dbdb9d5
Gabriel Aguiar De La Rocque <gde-la-r@c2r2s10.42porto.com>
2024-12-16 15:49:15 +0000

    started main.c

---
+++ sources/main.c
@@ -0,0 +1,27 @@
+#include ".././includes/push_swap.h"
+
+int	main(int ac, char **av)
+{
+	t_stack	*a;
+	t_stack	*b;
+	char	**res;
+
+	a = NULL;
+	b = NULL;
+	if (ac == 1 || (ac == 2 && !av[1][0]))
+		return (1);
+	else if (ac == 2)
+		res = ft_split(av[1], ' ');
+	ft_init_a(&a, res + 1);
+	if (!ft_sorted(a))
+	{
+		if (ft_stack_len(a) == 2)
+			sa(&a, false);
+		else if (ft_stack_len(a) == 3)
+			ft_sort_three(&a);
+		else
+			ft_sort_stacks(&a, &b);
+	}
+	free_stack(&a);
+	return (0);
+}
