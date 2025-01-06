0c27b7e1439e46ef4f199922c5d239cff7113de6
larocqueg <portugalgabriel2021@gmail.com>
2025-01-06 12:39:16 +0000

    swap ok (remaining sa, sb and ss)

--- sources/commands/swap.c
+++ sources/commands/swap.c
@@ -6,8 +6,18 @@
 /*   By: gde-la-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 13:52:22 by gde-la-r          #+#    #+#             */
-/*   Updated: 2024/12/27 13:52:23 by gde-la-r         ###   ########.fr       */
+/*   Updated: 2025/01/06 12:37:33 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+static	void	swap(t_stack **stack)
+{
+	t_stack	*temp;
 
+	if (!(*stack) || !(*stack)->next)
+		return ;
+	temp = (*stack)->next;
+	(*stack)->next = temp->next;
+	temp->next = *stack;
+	*stack = temp;
+}
