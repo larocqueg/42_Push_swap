02052dd25bbc169269c3d69e995eaeb3599cca63
larocqueg <portugalgabriel2021@gmail.com>
2025-01-08 18:12:53 +0000

    sudo code for sort_utils

--- sources/sort_stacks_utils.c
+++ sources/sort_stacks_utils.c
@@ -6,8 +6,18 @@
 /*   By: gde-la-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 17:50:25 by gde-la-r          #+#    #+#             */
-/*   Updated: 2025/01/08 17:50:36 by gde-la-r         ###   ########.fr       */
+/*   Updated: 2025/01/08 18:07:57 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+// Set index
+
+// Set cost
+
+// Find cheapest
+
+// Set median
+
+// above_median ?
