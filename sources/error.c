64c60ff3c0aafbed7b0835d72f7afcd8ab2aaeeb
larocqueg <portugalgabriel2021@gmail.com>
2025-05-22 19:41:45 +0100

    removed verifications that was not needed

--- sources/error.c
+++ sources/error.c
@@ -26,48 +26,6 @@ int	ft_error(t_stack *a)
 	return (ft_putstr_fd("Error\n", 2), -1);
 }
 
-static int	only_zero(char *str)
-{
-	int	i;
-
-	i = 0;
-	if (str[i] == '+' || str[i] == '-')
-		i++;
-	if (!str[i])
-		return (0);
-	while (str[i])
-	{
-		if (str[i] != '0')
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-static int	valid_size(char **arr)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (arr[i])
-	{
-		j = 0;
-		while (arr[i][j])
-		{
-			if (arr[i][j] == '-' && ft_strlen(arr[i]) > 20
-				&& !only_zero(arr[i]))
-				return (0);
-			else if (j == 0 && ft_strlen(arr[i]) > 19 && !only_zero(arr[i]))
-				return (0);
-			else
-				break ;
-		}
-		i++;
-	}
-	return (1);
-}
-
 int	only_numbers(char **arr)
 {
 	int	i;
@@ -81,9 +39,7 @@ int	only_numbers(char **arr)
 		{
 			if (arr[i][j] == '-' || arr[i][j] == '+')
 			{
-				if (!(arr[i][j + 1] >= '0' && arr[i][j + 1] <= '9'))
-					return (0);
-				else if (j > 1 && arr[i][j - 1] >= 0 && arr[i][j -1] <= 9)
+				if (j > 0)
 					return (0);
 			}
 			else if (!(arr[i][j] >= '0' && arr[i][j] <= '9'))
@@ -92,8 +48,6 @@ int	only_numbers(char **arr)
 		}
 		i++;
 	}
-	if (valid_size(arr) == 0)
-		return (0);
 	return (1);
 }
 
